ac17a085336cf8961e231e292a9b14fedc47b1b3
Yuhan Shao <yuhan@Yuhans-MacBook-Pro.local>
2018-11-25 16:58:14 -0500

    commit

--- dog_smallVGG.py
+++ dog_smallVGG.py
@@ -1,58 +1,43 @@
 import pickle
-from keras.models import Sequential
-from keras.layers import Convolution2D
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
-from keras.layers import Dense
-from keras.layers import Dense, Dropout, Activation, Flatten
+
+from keras.applications.vgg16 import VGG16
+from keras.layers import Input, Flatten, Dense
+from keras.models import Model
 from keras.preprocessing.image import ImageDataGenerator
-from keras.preprocessing import image
-from keras.optimizers import SGD, Adam
-from keras import optimizers
-import numpy as np
-from keras.applications import VGG19
+from keras.optimizers import SGD
 
 
-"""
-Parameters
-"""
 img_width, img_height = 64, 64
-batch_size = 32
-samples_per_epoch = 2000
-epochs = 30
-validation_steps = 200
+batch_size = 16
+samples_per_epoch = 2500
+epochs = 20
+validation_steps = 300
 nb_filters1 = 32
 nb_filters2 = 64
 conv1_size = 3
 conv2_size = 2
 pool_size = 2
 classes_num = 5
-lr = 0.0004     # learning rate
+lr = 0.0004
 
-classifier = Sequential()
-classifier.add(Convolution2D(32, kernel_size=(3, 3),padding='same',input_shape=(64, 64, 3)))
-classifier.add(Activation('relu'))
-classifier.add(Convolution2D(64, (3, 3)))
-classifier.add(Activation('relu'))
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-classifier.add(Dropout(0.25))
+model_vgg16_conv = VGG16(weights='imagenet', include_top=False)
+model_vgg16_conv.summary()
 
-classifier.add(Convolution2D(64,(3, 3), padding='same'))
-classifier.add(Activation('relu'))
-classifier.add(Convolution2D(64, 3, 3))
-classifier.add(Activation('relu'))
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-classifier.add(Dropout(0.25))
+input = Input(shape=(64,64,3),name = 'image_input')
 
-classifier.add(Flatten())
-classifier.add(Dense(512))
-classifier.add(Activation('relu'))
-classifier.add(Dropout(0.5))
-classifier.add(Dense(5))
-classifier.add(Activation('softmax'))
-classifier.compile(loss='categorical_crossentropy',
-              optimizer=optimizers.RMSprop(lr=lr),
-              metrics=['accuracy'])
+output_vgg16_conv = model_vgg16_conv(input)
+
+x = Flatten(name='flatten')(output_vgg16_conv)
+x = Dense(512, activation='relu', name='fc1')(x)
+x = Dense(512, activation='relu', name='fc2')(x)
+x = Dense(5, activation='softmax', name='predictions')(x)
+
+my_model = Model(input=input, output=x)
+
+my_model.summary()
+
+sdg = SGD(lr=0.001, clipnorm=1.)
+my_model.compile(loss='categorical_crossentropy', optimizer=sdg, metrics=['accuracy'])
 
 train_datagen = ImageDataGenerator(
     rescale=1. / 255,
@@ -62,23 +47,25 @@ train_datagen = ImageDataGenerator(
 
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 
-
 train_generator = train_datagen.flow_from_directory(
-    'dog_cat_dataset/dog/train',
-    target_size=(img_height, img_width),
-    batch_size=batch_size,
-    class_mode='categorical')
+  "dog_cat_dataset/dog/train",
+  target_size=(img_height, img_width),
+  batch_size=batch_size,
+  class_mode='categorical')
 
 validation_generator = test_datagen.flow_from_directory(
-    'dog_cat_dataset/dog/test',
-    target_size=(img_height, img_width),
-    batch_size=batch_size,
-    class_mode='categorical')
+  "dog_cat_dataset/dog/test",
+  target_size=(img_height, img_width),
+  batch_size=batch_size,
+  class_mode='categorical')
 
-classifier.fit_generator(
-    train_generator,
-    samples_per_epoch = samples_per_epoch,
-    epochs=epochs,
-    validation_data=validation_generator,
-    validation_steps=validation_steps)
+my_model.fit_generator(
+  train_generator,
+  samples_per_epoch = samples_per_epoch,
+  epochs=epochs,
+  validation_data=validation_generator,
+  validation_steps=validation_steps)
 
+
+with open('classifier.pickle', 'wb') as handle:
+    pickle.dump(my_model, handle, protocol=pickle.HIGHEST_PROTOCOL)
